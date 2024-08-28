34c5bb18c95979755a4af972196782f2f99e8a87
tharsis-soares <tharsissoares@hotmail.com>
2024-08-28 01:23:01 -0300

    'changes-fixed'

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from flask import Flask, render_template
+import requests
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
-@app.route('/')
-def get_all_posts():
-    return render_template('index.html')
+@app.route("/")
+def home():
+    response = requests.get('https://api.npoint.io/674f5423f73deab1e9a7')
+    data = response.json()
+    print(data)
+    return render_template('index.html', json_data=data)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
