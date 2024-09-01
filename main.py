44c8dd58d6cde01c119fe961bfeb199040832e85
tharsis-soares <tharsissoares@hotmail.com>
2024-09-01 07:55:46 -0300

    Contact Form Adjusts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
@@ -16,9 +16,32 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=['POST', 'GET'])
 def contact():
-    return render_template('contact.html')
+    messageInfo = False
+    if request.method == "POST":
+        data = request.form
+        name = data["name"]
+        # phone = datarequest.form.get("phone")
+        # message = request.form.get("message")
+        # print(f"Name: {name}\nPhone: {phone}\nMessage: {message}")
+        success_message = "Successfully sent your message."
+        messageInfo = True
+    elif request.method == "GET" and request.args:
+        messageInfo = True
+    return render_template('contact.html', success=messageInfo)
+
+# @app.route("/contact", methods=["GET", "POST"])
+# def contact():
+#     if request.method == 'GET':
+#         return render_template("contact.html", form_submitted=False)
+#     info = request.form
+#     print(info["name"])
+#     print(info["email"])
+#     print(info["phone"])
+#     print(info["message"])
+#     form_submitted = True
+#     return render_template('contact.html', form_submitted=form_submitted)
 
 @app.route("/post/<int:index>")
 def show_post(index):
